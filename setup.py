893bfc2e31cf09c69d2365445beac0439ed14860
Filip Pobořil <tsuki@fpob.eu>
2018-03-15 22:07:07 +0100

    Fix setup script

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup
 
-import logicoma
+import os
+import re
 
+pkg_name = 'logicoma'
+
+__dir__ = os.path.dirname(__file__)
+with open(os.path.join(__dir__, pkg_name, '__init__.py')) as f:
+    for line in f:
+        if re.match(r'__version__', line):
+            exec(line)
+            break
 
 setup(
-    name='logicoma',
-    version=logicoma.__version__,
+    name=pkg_name,
+    version=__version__,
     description='Package for creating simple web crawlers.',
     author='Filip Pobořil',
     author_email='tsuki@fpob.eu',
